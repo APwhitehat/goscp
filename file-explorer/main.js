3138f3481d53b2232f99c98fac619da24c87b028
Mayank Sharma <msharma@iitk.ac.in>
2018-11-15 23:38:11 +0530

    Added Electron GUI to backend

---
+++ file-explorer/main.js
@@ -0,0 +1,99 @@
+const {app, BrowserWindow, ipcMain} = require('electron');
+const electron = require('electron');
+const node_ssh = require('node-ssh');
+const fs = require('fs');
+
+function fs_writeFile(fileName, data) {
+	return new Promise(function(resolve, reject) {
+		fs.writeFile(fileName, data, 'utf-8', function(err) {
+			if (err) reject(err);
+			else resolve(data);
+		});
+	});
+}
+function fs_readFile(fileName, data) {
+	return new Promise(function(resolve, reject) {
+		fs.readFile(fileName, function(err, data) {
+			if (err) reject(err);
+			else resolve(data);
+		});
+	});
+}
+
+var ssh = new node_ssh();
+// Enable live reload for Electron too
+require('electron-reload')(__dirname, {
+	// Note that the path to electron may vary according to the main file
+	electron: require(`${__dirname}/node_modules/electron`)
+});
+let mainWindow;
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+	if (process.platform != 'darwin')
+		app.quit();
+});
+
+// This method will be called when Electron has done everything
+// initialization and ready for creating browser windows.
+app.on('ready', function() {
+	// Create the browser window.
+	mainWindow = new BrowserWindow({width: 800, height: 600});
+	credentialsFile = "./credentials.txt";
+
+	// and load the index.html of the app.
+	mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+	mainWindow.webContents.once('dom-ready', () => {
+		fs_readFile(credentialsFile)
+			.then((data) => JSON.parse(data))
+			.then((data) => {
+				console.log(data);
+				mainWindow.webContents.send('restoreForm', data);
+				console.log("Restored Form Values");
+			})
+			.catch(console.error);
+
+
+	})
+	// Emitted when the window is closed.
+	mainWindow.on('closed', function() {
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+		mainWindow = null;
+	});
+
+
+
+	ipcMain.on('getDirectory', (evt, data) => {
+		console.log(data);
+		delete(data.password);
+		if (data.saveValue) {
+			fs_writeFile(credentialsFile, JSON.stringify(data))
+				.then((val) => console.log("Write Successful"))
+				.catch(console.error);
+		}
+	})
+	/*  ssh.connect({
+	 *    host: 'localhost',
+	 *    username: 'msharma',
+	 *    privateKey: '/home/msharma/.ssh/msharma'
+	 *  })
+	 *    .then(() => {
+	 *      let homeDir = '/home/msharma/'
+	 *      ssh.getFile('/home/msharma/Downloads/index.html',`${homeDir}/Public/foo` )	
+	 *        .then((contents)=> {
+	 *
+	 *          console.log("The File's contents were successfully downloaded");
+	 *        })
+	 *      ssh.execCommand('ls -la', {
+	 *        cwd: `${homeDir}/Public` 
+	 *      })
+	 *        .then((result) => {
+	 *          console.log(result.stdout.split('\n'));
+	 *        })
+	 *        .catch((err) => console.error(err))
+	 *    })
+	 *    .catch((err) => console.error(err))*/
+});
---
+++ file-explorer/script.js
@@ -0,0 +1,127 @@
+global.$ = $;
+const electron = require('electron');
+const remote  = electron.remote;
+const {Menu, BrowserWindow, MenuItem, shell} = remote;
+const ipc = electron.ipcRenderer;
+
+var abar = require('address_bar');
+var folder_view = require('folder_view');
+
+function RadioOnClickHandler() {
+	let form = document.getElementById('form');
+	[form['password'].disabled, form['privKeyPath'].disabled] = [form['privKeyPath'].disabled, form['password'].disabled] ;
+}
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+		var forms = document.getElementsByClassName('needs-validation');
+		let rbutton1 = document.getElementById('rbutton1');
+		let rbutton2 = document.getElementById('rbutton2');
+		rbutton1.onclick = RadioOnClickHandler;
+		rbutton2.onclick = RadioOnClickHandler;
+
+		ipc.on('restoreForm', (evt, data) => {
+			console.log(data);
+			let form = document.getElementById('form');
+			form['hostname'].value = data.hostname | "";
+			form['privKeyPath'].value = data.privKeyPath | "";
+			form['checkbox'].checked = data.saveValue | "";
+		})
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+// append default actions to menu for OSX
+function formSubmit(){
+	let form = document.getElementById('form');
+	console.log(form);
+	let formData = {
+		hostname: form['hostname'].value,
+		username: form['username'].value,
+		privKeyPath: form['privKeyPath'].value,
+		password: form['password'].value,
+		src: form['src'].value,
+		dst: form['dest'].value,
+		saveValue: form['checkbox'].checked
+	}
+	console.log(formData);
+	/*alert('Done!');*/
+	ipc.send('getDirectory', formData);
+	if (formData.hostname == "" || formData.username == "" || formData.src == "" || formData.dest == ""){
+		console.log("Form not filled!");
+		return;
+	}
+	let endPoint = 'http://localhost:8080/scp';
+
+	fetch(endPoint, {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, cors, *same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, same-origin, *omit
+        headers: {
+            "Content-Type": "application/json; charset=utf-8",
+        },
+        body: JSON.stringify(formData), // body data type must match "Content-Type" header
+	})
+		.then((val) => {
+			console.log(val);
+		})
+		.catch(console.error);
+	return false;
+}
+var initMenu = function () {
+	try {
+		var nativeMenuBar = new Menu();
+		if (process.platform == "darwin") {
+			nativeMenuBar.createMacBuiltin && nativeMenuBar.createMacBuiltin("FileExplorer");
+		}
+	} catch (error) {
+		console.error(error);
+		setTimeout(function () { throw error }, 1);
+	}
+};
+
+var aboutWindow = null;
+var credentialsWindow = null;
+var App = {
+	// show "about" window
+	about: function () {
+		var params = {toolbar: false, resizable: false, show: true, height: 150, width: 400};
+		aboutWindow = new BrowserWindow(params);
+		aboutWindow.loadURL('file://' + __dirname + '/about.html');
+	},
+	// change folder for sidebar links
+	cd: function (anchor) {
+		anchor = $(anchor);
+
+		$('#sidebar li').removeClass('active');
+		$('#sidebar i').removeClass('icon-white');
+
+		anchor.closest('li').addClass('active');
+		anchor.find('i').addClass('icon-white');
+
+		this.setPath(anchor.attr('nw-path'));
+	},
+
+	// set path for file explorer
+	setPath: function (path) {
+		if (path.indexOf('~') == 0) {
+			path = path.replace('~', process.env['HOME']);
+		}
+		this.folder.open(path);
+		this.addressbar.set(path);
+	}
+};
+
+$(document).ready(function() {
+	initMenu();
+});
